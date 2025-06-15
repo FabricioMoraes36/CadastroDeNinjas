c5c2bc604df93ccd8ce55544f57872eb5c41e765
Fabricio Moraes <Fabricio36365@outlook.com>
2025-06-14 23:29:17 -0300

    "Meu primeiro commit com o controller e model criados"

---
+++ src/main/java/com/dev/batismoDeJava/cadastroNinja/CadastroDeNinjas/NinjaController.java
@@ -0,0 +1,15 @@
+package com.dev.batismoDeJava.cadastroNinja.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class NinjaController {
+
+    @GetMapping("/BoasVindas")
+    public void mensagemController(){
+        System.out.println("Essa é minha primeira mensagem");
+    }
+}
---
+++ src/main/java/com/dev/batismoDeJava/cadastroNinja/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,47 @@
+package com.dev.batismoDeJava.cadastroNinja.CadastroDeNinjas;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class NinjaModel {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
