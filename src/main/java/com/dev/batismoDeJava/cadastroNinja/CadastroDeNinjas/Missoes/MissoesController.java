28596e2cc1b18d544827762be61c7fe1c1d52dc4
Fabricio Moraes <Fabricio36365@outlook.com>
2025-06-29 22:00:46 -0300

    feat: criação de rotas para o controller com mock

--- src/main/java/com/dev/batismoDeJava/cadastroNinja/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/com/dev/batismoDeJava/cadastroNinja/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,45 @@
 package com.dev.batismoDeJava.cadastroNinja.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
 public class MissoesController {
+    public String boasVindas(){
+        return "Boas vindas as missoes";
+    }
+    //Endpoint - criar missão
+    //POST - REQUISIÇÃO PARA CRIAR MISSÃO
+    @PostMapping("/criarMissao")
+    public String criarMissao(){
+        return "Missão criada com sucesso!!";
+    }
+
+    //Endpoint - procurar missão por id
+    //GET - REQUISIÇÃO PARA MOSTRAR MISSÃO POR ID
+    @GetMapping("/procurarPorId")
+    public String procurarMissaoPorId(){
+        return "Missão procurada por Id";
+    }
+
+    //Endpoint - mostrar todas as missões
+    //GET - REQUISIÇÃO PARA MOSTRAR TODAS MISSÕES
+    @GetMapping("/todos")
+    public String mostrarTodasAsMissoes(){
+        return "Mostrar todas as missões";
+    }
+
+    //Endpoint - atualizar a missão por id
+    //PUT - REQUISIÇÃO PARA ATUALIZAR A MISSÃO POR ID
+    @PutMapping("/atualizarPorId")
+    public String atualizarMissaoPorId(){
+        return "Missão atualizada por id";
+    }
+    //Endpoint - deletar a missão por id
+    //DELETE - REQUISIÇÃO PARA DELETAR MISSÃO POR ID
+    @DeleteMapping("/deletarId")
+    public String deletarMissaoPorId(){
+        return "Missão deletada por id";
+    }
+
 }
