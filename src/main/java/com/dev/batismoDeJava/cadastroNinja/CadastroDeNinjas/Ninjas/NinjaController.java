4e2c1c86d7e4f7c0092d2d7212d1080dc525e1d4
Fabricio Moraes <Fabricio36365@outlook.com>
2025-06-29 21:35:09 -0300

    feat: Atualiza o controller com mock de dados(criação dos endpoints)

--- src/main/java/com/dev/batismoDeJava/cadastroNinja/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/com/dev/batismoDeJava/cadastroNinja/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,7 +1,5 @@
 package com.dev.batismoDeJava.cadastroNinja.CadastroDeNinjas.Ninjas;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,4 +9,39 @@ public class NinjaController {
     public void mensagemController(){
         System.out.println("Essa é minha primeira mensagem");
     }
+
+    //Endpoint - Criar ninjas
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja criado com sucesso!!";
+    }
+
+    //Endpoint - mostrar ninjas por id
+    @GetMapping("/todosId")
+    public String buscaNinjaPorId(){
+        return "Mostrar ninja por Id";
+    }
+
+    //Endpoint - Mostrar os ninjas
+    @GetMapping("/todos")
+    public String buscaNinja(){
+        return "Mostrar ninjas";
+    }
+
+    //Endpoint - alterar dados do ninja por id
+    @PutMapping("/alterarPorId")
+    public String alterarNinjaPorId(){
+        return "Dados alterados com sucesso pelo id";
+    }
+
+    //Endpoint - Deletar ninjas por id
+    @DeleteMapping("/deletarPorId")
+    public String deletarNinja(){
+        return "Ninja deletado com sucesso pelo id";
+    }
+
+
+
+
+
 }
