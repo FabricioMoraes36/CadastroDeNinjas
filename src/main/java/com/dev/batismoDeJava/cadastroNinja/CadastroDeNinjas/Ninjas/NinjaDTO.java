6d3f55fc9050531dfa6df95279d71dd6a2191025
Fabricio Moraes <Fabricio36365@outlook.com>
2025-07-05 16:00:41 -0300

    chore: inclui a versão do DTO pela entidade,e mapeia manualmente no NinjaMapper os caminhos entidadexDTO(NinjaModel x NinjaDTO)

--- src/main/java/com/dev/batismoDeJava/cadastroNinja/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/com/dev/batismoDeJava/cadastroNinja/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,33 @@
 package com.dev.batismoDeJava.cadastroNinja.CadastroDeNinjas.Ninjas;
 
+import com.dev.batismoDeJava.cadastroNinja.CadastroDeNinjas.Missoes.MissoesModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    //atributos sem annotations referentes ao banco de dados
+
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String imgUrl;
+
+    private int idade;
+
+    private MissoesModel missoes;
+
+    //A migration que haviamos feito pra coluna rank agora pode ser declarado no dto como um atributo igual aos outros
+    private String rank;
+
+
+
 }
--- src/main/java/com/dev/batismoDeJava/cadastroNinja/CadastroDeNinjas/Ninjas/NinjaMapper.java
+++ src/main/java/com/dev/batismoDeJava/cadastroNinja/CadastroDeNinjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,34 @@
 package com.dev.batismoDeJava.cadastroNinja.CadastroDeNinjas.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    //entidade para DTO
+    public NinjaModel map(NinjaDTO ninjaDTO){
+        NinjaModel ninjaModel = new NinjaModel();
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setNome(ninjaDTO.getNome());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setImgUrl(ninjaDTO.getImgUrl());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+        ninjaModel.setRank(ninjaDTO.getRank());
+
+        return ninjaModel;
+    }
+    //DTO para entidade
+    public NinjaDTO map(NinjaModel ninjaModel){
+        NinjaDTO ninjaDTO = new NinjaDTO();
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setNome(ninjaModel.getNome());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setImgUrl(ninjaModel.getImgUrl());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+        ninjaDTO.setRank(ninjaModel.getRank());
+
+        return ninjaDTO;
+    }
 }
