f6f3e065927ec7c203520b7a75e5b60bd819207b
Fabricio Moraes <Fabricio36365@outlook.com>
2025-07-05 16:07:41 -0300

    feat: Inclui DTO como abstração da entidade(NinjaModel) na camada Service e Controller

--- src/main/java/com/dev/batismoDeJava/cadastroNinja/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/com/dev/batismoDeJava/cadastroNinja/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -22,8 +22,8 @@ public class NinjaController {
 
     //Endpoint - Criar ninjas
     @PostMapping("/criar")
-    public NinjaModel criarNinja(@RequestBody NinjaModel ninjaModel) {
-        return ninjaService.criarNinja(ninjaModel);
+    public NinjaDTO criarNinja(@RequestBody NinjaDTO ninja) {
+        return ninjaService.criarNinja(ninja);
     }
 
     //Endpoint - mostrar ninjas por id
--- src/main/java/com/dev/batismoDeJava/cadastroNinja/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/com/dev/batismoDeJava/cadastroNinja/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -9,10 +9,17 @@ import java.util.Optional;
 @Service
 public class NinjaService {
     //podemos usar o autowired mas é uma boa pratica criar o construtor
-    @Autowired
+    //@Autowired
     //modificador de acesso - classe de repository - nome da variavel(é um padrão ter o mesmo nome do repository)
     //Injeção de dependencia do NinjaRepository pra usar os metodos ja prontos do jpa
     private NinjaRepository ninjaRepository;
+    //instancia do mapper
+    private NinjaMapper ninjaMapper;
+    //recriamos o construtor com o ninjaMapper ja incluso
+    public NinjaService(NinjaRepository ninjaRepository, NinjaMapper ninjaMapper) {
+        this.ninjaRepository = ninjaRepository;
+        this.ninjaMapper = ninjaMapper;
+    }
 
     //listar todos os ninjas
     public List<NinjaModel> listarNinjas() {
@@ -27,8 +34,19 @@ public class NinjaService {
     }
 
     //criar um novo ninja
-    public NinjaModel criarNinja(NinjaModel ninja) {
-        return ninjaRepository.save(ninja);
+     //- cria um metodo do tipo NinjaDTO,e o paramentro é um NinjaDTO chamado ninjaDTO
+     //- instanciamos o tipo ninjaModel chamado ninja1 que recebe um novo NinjaMapper,usa o metodo map e passamos o ninjaDTO do parametro,mapeamos o model como DTO
+     //- atribuimos ao ninja o valor de = ninjaRepository.save(ninja); ele esta salvando a instancia do nosso ninjaModel que chamamos de ninja
+     //- depois so retornamos a instancia do nosso mapper(ninjaMapper que ate fizemos o construtor junto com o ninjaRepository la em cima)
+     //- e o ninjaMapper usa o metodo pra mapear o ninja,que é a instancia do nosso model
+
+    public NinjaDTO criarNinja(NinjaDTO ninjaDTO) {
+        //- basicamente,transformamos o model em dto
+        NinjaModel ninja = new NinjaMapper().map(ninjaDTO);
+        //- salvamos usando o ninjaRepository
+        ninja = ninjaRepository.save(ninja);
+        //- aqui convertemos de volta,agora de DTO para model
+        return ninjaMapper.map(ninja);
     }
 
     //deletar um novo ninja por id - tem que ser void
