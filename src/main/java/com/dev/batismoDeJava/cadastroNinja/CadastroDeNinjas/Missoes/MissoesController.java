7fba7314b7fc04d108f5182e4977c559380bc429
Fabricio Moraes <Fabricio36365@outlook.com>
2025-07-09 16:32:49 -0300

    feat: Criação da rota PUT para alterar as missões ja cadastradas e padronização das rotas

--- src/main/java/com/dev/batismoDeJava/cadastroNinja/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/com/dev/batismoDeJava/cadastroNinja/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,14 +1,15 @@
 package com.dev.batismoDeJava.cadastroNinja.CadastroDeNinjas.Missoes;
-
-import com.dev.batismoDeJava.cadastroNinja.CadastroDeNinjas.Ninjas.NinjaModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
 @RequestMapping("missoes")
 public class MissoesController {
 
+    @Autowired
     private MissoesService missoesService;
 
     public MissoesController(MissoesService missoesService) {
@@ -23,37 +24,53 @@ public class MissoesController {
     //POST - REQUISIÇÃO PARA CRIAR MISSÃO
     //Precisa de um json correto para criar no db
     @PostMapping("/criar")
-    public MissoesModel criarMissao(@RequestBody MissoesModel missao){
-        return missoesService.criarMissao(missao);
+    public ResponseEntity<String> criarMissao(@RequestBody MissoesDTO missao){
+     MissoesDTO novaMissao = missoesService.criarMissao(missao);
+     return ResponseEntity.status(HttpStatus.CREATED).body("Missão criada com sucesso");
     }
 
     //Endpoint - procurar missão por id
     //GET - REQUISIÇÃO PARA MOSTRAR MISSÃO POR ID
     //@PathVariable indica que Long id fará parte da url
     @GetMapping("/todos/{id}")
-    public MissoesModel listarMissoesPorId(@PathVariable Long id){
-        return missoesService.listarMissoesPorId(id);
+    public ResponseEntity<?> missaoPorId (@PathVariable Long id){
+       MissoesDTO missao = missoesService.listarMissoesPorId(id);
+       if (missao != null){
+           return ResponseEntity.ok(missao);
+       }
+       return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Missão não encontrado no id fornecido" + id);
     }
 
     //Endpoint - mostrar todas as missões
     //GET - REQUISIÇÃO PARA MOSTRAR TODAS MISSÕES
     @GetMapping("/todos")
-    public List<MissoesModel> listarMissoes(){
-        return missoesService.listarMissoes();
+    public ResponseEntity<List<MissoesDTO>> listarMissoes(){
+        List<MissoesDTO> missoes = missoesService.listarMissoes();
+        return ResponseEntity.ok(missoes);
     }
 
     //Endpoint - deletar a missão por id
     //DELETE - REQUISIÇÃO PARA DELETAR MISSÃO POR ID
     @DeleteMapping("/deletar/{id}")
-    public void deletarMissaoPorId(@PathVariable Long id){
-        missoesService.deletarMissaoPorId(id);
+    public ResponseEntity<String> deletarMissaoPorId(@PathVariable Long id){
+        if (missoesService.listarMissoesPorId(id) != null) {
+            missoesService.deletarMissaoPorId(id);
+            return ResponseEntity.ok("Ninja do id: " + id + " Deletado com sucesso");
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Missão não encontrado no id fornecido" + id);
+        }
     }
 
     //Endpoint - atualizar a missão por id
     //PUT - REQUISIÇÃO PARA ATUALIZAR A MISSÃO POR ID
-    @PutMapping("/alterarPorId")
-    public String atualizarMissaoPorId(){
-        return "Missão atualizada por id";
+    @PutMapping("/alterar/{id}")
+    public ResponseEntity<?> atualizarMissaoPorId(@PathVariable Long id,@RequestBody MissoesDTO missoesDTO){
+        MissoesDTO missao = missoesService.listarMissoesPorId(id);
+        if (missao != null){
+            MissoesDTO missaoAlterada = missoesService.alterarMissaoPorId(id,missoesDTO);
+            return ResponseEntity.ok(missaoAlterada);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Ninja não encontrado no id fornecido" + id);
     }
 
 }
--- src/main/java/com/dev/batismoDeJava/cadastroNinja/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/com/dev/batismoDeJava/cadastroNinja/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,37 +1,62 @@
 package com.dev.batismoDeJava.cadastroNinja.CadastroDeNinjas.Missoes;
 
+import com.dev.batismoDeJava.cadastroNinja.CadastroDeNinjas.Ninjas.NinjaModel;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class MissoesService {
     private MissoesRepository missoesRepository;
+    private MissoesMapper missoesMapper;
 
-    public MissoesService(MissoesRepository missoesRepository) {
+    public MissoesService(MissoesRepository missoesRepository, MissoesMapper missoesMapper) {
         this.missoesRepository = missoesRepository;
+        this.missoesMapper = missoesMapper;
     }
 
     //Listar todas as missões
-    public List<MissoesModel>listarMissoes(){
-        return missoesRepository.findAll();
+    public List<MissoesDTO>listarMissoes(){
+        List<MissoesModel>lista = missoesRepository.findAll();
+        return lista.stream()
+                .map(missoesMapper::map)
+                .collect(Collectors.toList());
+
     }
 
     //Listar por id
-    public MissoesModel listarMissoesPorId(Long id){
+    public MissoesDTO listarMissoesPorId(Long id){
         Optional<MissoesModel>listarPorId = missoesRepository.findById(id);
-        return listarPorId.orElse(null);
+        return listarPorId.map(missoesMapper::map).orElse(null);
     }
 
     //Criar missão
 
-    public MissoesModel criarMissao(MissoesModel missao){
-        return missoesRepository.save(missao);
+    public MissoesDTO criarMissao(@RequestBody MissoesDTO missoesDTO ){
+        MissoesModel missao = new MissoesMapper().map(missoesDTO);
+        missao = missoesRepository.save(missao);
+        return missoesMapper.map(missao);
     }
 
-    //Deletar missão por id
+    //Deletar missão por id *Delete não colocamos o DTO pq nenhum dado sensivel será exposto para os usuarios*
     public void deletarMissaoPorId(Long id){
         missoesRepository.deleteById(id);
+    }
+
+    //Alterar missão por id
+
+    public MissoesDTO alterarMissaoPorId(Long id, MissoesDTO missoesDTO){
+        Optional<MissoesModel> missao = missoesRepository.findById(id);
+        if (missao.isPresent()){
+            MissoesModel missaoAtt = missoesMapper.map(missoesDTO);
+            missaoAtt.setId(id);
+            MissoesModel missaoSalva = missoesRepository.save(missaoAtt);
+            return missoesMapper.map(missaoSalva);
+
+        }
+        return null;
     }
 }
